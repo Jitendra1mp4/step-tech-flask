94ba7ebd1eacf27a820ac36a1b8c63b06d595c55
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-08-11 21:42:10 +0530

    added logic to save user details

--- sampleApp/db_handler.py
+++ sampleApp/db_handler.py
@@ -1,2 +1,21 @@
 from sampleApp import mysql
 
+
+def save_user(form):
+    name = form.get("name")
+    email = form.get("email")
+    roll = form.get("role")
+
+    status = 1
+    try:
+        cur = mysql.connection.cursor()
+        qry = "INSERT INTO users(`name`, `email`,`role`) VALUES(%s, %s, %s)"
+        result = cur.execute(qry, (name, email, roll))
+        mysql.connection.commit()
+        if not result:
+            status = "Failed to save user detail"
+    except:
+        status = "Sorry! Error encountered while saving user details"
+    finally:
+        cur.close()
+    return status
--- sampleApp/routs.py
+++ sampleApp/routs.py
@@ -1,9 +1,10 @@
-from flask import render_template, request
+from flask import render_template, request,flash,redirect, url_for
 from sampleApp import app
+import sampleApp.db_handler as dbh
 
 
 @app.route("/")
-# @app.route("/home")
+@app.route("/home")
 def home_page():
     return render_template("home_page.html")
 
@@ -11,6 +12,18 @@ def home_page():
 @app.route("/hello")
 def hello_page():
     return "<h1>Hello World!</h1>"
+
+
+@app.route("/adduser", methods=["GET", "POST"])
+def adduser_page():
+    if request.method == "POST":
+        print(request.form)
+        status = dbh.save_user(request.form)
+        if status: 
+            return redirect(url_for('users_page'))
+        else:
+            return f'<h2 class="text-danger">{status}</h2>'
+    return render_template("adduser_page.html")
 
 
 @app.route("/users")
